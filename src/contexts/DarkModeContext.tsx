e514aa8eb9bd4ccadbac1de1552b82128272c1e6
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-03 20:13:51 +0530

    Removed the insertion and removal of redundant class for theme toggling

--- src/contexts/DarkModeContext.tsx
+++ src/contexts/DarkModeContext.tsx
@@ -21,9 +21,7 @@ function DarkModeProvider({ children }: DarkModeProviderProps) {
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark-mode");
-      document.documentElement.classList.remove("light-mode");
     } else {
-      document.documentElement.classList.add("light-mode");
       document.documentElement.classList.remove("dark-mode");
     }
   }, [isDarkMode]);

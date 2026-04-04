1ea50271d6981387889396f1703f5a398afb56e7
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-04 21:30:52 +0530

    Added the /dashboard page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { DarkModeProvider } from "./contexts/DarkModeContext";
 import AppLayout from "./components/ui/AppLayout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>

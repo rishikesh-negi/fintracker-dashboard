7997de566b4d2a1e539c03d92d5df031affc6db9
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-06 23:40:54 +0530

    Changed icons

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,48 +8,51 @@ import SummaryCard from "../components/SummaryCard";
 import { selectBalance, selectDashboardData } from "../store/accountSlice";
 import { useAppSelector } from "../store/storeHooks";
 import { currencyFormatter } from "../utils/currencyFormatter";
+import { MdAccountBalanceWallet, MdCurrencyRupee, MdPayments, MdSavings } from "react-icons/md";
 
 export default function Dashboard() {
   const { summaries, expensesByCategory, chartData, recentTransactions } =
     useAppSelector(selectDashboardData);
   const balance = useAppSelector(selectBalance);
 
-  const iconClasses = "text-3xl text-text/50";
+  const iconClasses = "text-3xl text-faint-text";
 
   return (
-    <section className="w-full h-full flex flex-col gap-4">
-      <DashboardFilter />
-      <div className="w-full grid grid-cols-4 lg:grid-cols-8 grid-rows-[repeat(8,auto)] gap-x-2 lg:gap-x-3 gap-y-2 lg:gap-y-3">
-        <SummaryCard
-          title="balance"
-          icon={<LuWallet className={iconClasses} />}
-          figure={currencyFormatter.format(balance)}
-        />
-        <SummaryCard
-          title="expenses"
-          icon={<LuHandCoins className={`${iconClasses}`} />}
-          figure={currencyFormatter.format(summaries.expensesSummary.expenses)}
-          change={summaries.expensesSummary.percentChangeExpenses}
-        />
-        <SummaryCard
-          title="income"
-          icon={<HiOutlineBanknotes className={iconClasses} />}
-          figure={currencyFormatter.format(summaries.incomeSummary.income)}
-          change={summaries.incomeSummary.percentChangeIncome}
-        />
-        <SummaryCard
-          title="savings rate"
-          icon={<LuPercent className={iconClasses} />}
-          figure={`${summaries.savingsRateSummary.savingsRate}%`}
-          change={summaries.savingsRateSummary.percentChangeSavingsRate}
-        />
+    <>
+      <section className="w-full h-full flex flex-col gap-4">
+        <DashboardFilter />
+        <div className="w-full grid grid-cols-4 lg:grid-cols-8 grid-rows-[repeat(8,auto)] gap-x-2 lg:gap-x-3 gap-y-2 lg:gap-y-3">
+          <SummaryCard
+            title="balance"
+            icon={<MdAccountBalanceWallet className={iconClasses} />}
+            figure={currencyFormatter.format(balance)}
+          />
+          <SummaryCard
+            title="expenses"
+            icon={<MdPayments className={`${iconClasses}`} />}
+            figure={currencyFormatter.format(summaries.expensesSummary.expenses)}
+            change={summaries.expensesSummary.percentChangeExpenses}
+          />
+          <SummaryCard
+            title="income"
+            icon={<MdCurrencyRupee className={iconClasses} />}
+            figure={currencyFormatter.format(summaries.incomeSummary.income)}
+            change={summaries.incomeSummary.percentChangeIncome}
+          />
+          <SummaryCard
+            title="savings rate"
+            icon={<MdSavings className={iconClasses} />}
+            figure={`${summaries.savingsRateSummary.savingsRate}%`}
+            change={summaries.savingsRateSummary.percentChangeSavingsRate}
+          />
 
-        <FinancesChart chartData={chartData!} />
+          <FinancesChart chartData={chartData!} />
 
-        <ExpensesBreakdownPieChart chartData={expensesByCategory} />
+          <ExpensesBreakdownPieChart chartData={expensesByCategory} />
 
-        <RecentTransactions recentTransactions={recentTransactions} />
-      </div>
-    </section>
+          <RecentTransactions recentTransactions={recentTransactions} />
+        </div>
+      </section>
+    </>
   );
 }

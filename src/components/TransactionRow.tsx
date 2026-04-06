0d32602f26ce367001070417dbec52aeb2ed34f5
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-06 23:17:49 +0530

    Fixed layout and spacing of Transaction row

--- src/components/TransactionRow.tsx
+++ src/components/TransactionRow.tsx
@@ -14,7 +14,7 @@ export default function TransactionRow({ transaction, ...props }: RecentTransact
   return (
     <li
       {...props}
-      className="grid w-full grid-cols-[1.2fr_3fr_1fr_2fr] gap-3 items-center text-xs px-2 py-2">
+      className="grid w-full grid-cols-[1.2fr_3fr_1fr_1fr] gap-3 items-center text-xs px-2 py-2">
       <span className="text-xs">{format(date, "MMM dd")}</span>
       <span className="text-xs">{description}</span>
       <Tag

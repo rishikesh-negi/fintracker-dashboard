2201e99c1b87258ab2de322c4e83a979db459181
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-06 23:48:19 +0530

    Adjusted the letter spacing of change indicator text

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ bgColor, text, ...props }: TagProps) {
   return (
     <span
       {...props}
-      className={`${bgColor} w-fit flex items-center justify-center text-[0.6rem] font-bold px-2 py-0.5 rounded-full text-light-200 uppercase`}>
+      className={`${bgColor} w-fit flex items-center justify-center text-[0.6rem] font-bold px-2 py-0.5 rounded-full text-light-100 uppercase`}>
       {text}
     </span>
   );

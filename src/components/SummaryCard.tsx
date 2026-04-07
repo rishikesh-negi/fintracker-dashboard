8f36d4b04acb391d8536412ad657836cb528a0b5
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-07 16:47:00 +0530

    Ported reusable classes from local variables to meta usitlity classes

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -8,15 +8,10 @@ type SummaryCardProps = ComponentPropsWithoutRef<"div"> & {
 };
 
 export default function SummaryCard({ title, icon, figure, change, ...props }: SummaryCardProps) {
-  const summaryCardClasses =
-    "col-span-2 row-span-1 relative p-3 sm:p-4 bg-component-bg rounded-md shadow-md";
-
-  const iconContainerClasses = `w-12 md:w-12 aspect-square md:h-12 rounded-full flex items-center justify-center bg-backdrop shadow-[inset_0.1rem_0.15rem_0.2rem_0.1rem_rgba(0,0,0,0.1)]`;
-
   return (
-    <div {...props} className={summaryCardClasses}>
+    <div {...props} className="summary-card">
       <div className="w-full h-full flex items-center gap-2 sm:gap-3">
-        <div className={iconContainerClasses}>{icon}</div>
+        <div className="summary-icon-container">{icon}</div>
         <div className="h-full flex flex-col gap-0.5 justify-center">
           <p className="text-[0.6rem] md:text-xs text-faint-text uppercase tracking-wider font-extrabold">
             {title}

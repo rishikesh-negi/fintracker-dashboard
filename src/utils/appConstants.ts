a68f62b9fddb5483f98aee4f23140dd71499349f
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-07 19:58:00 +0530

    Added per-page row count for transactions list pagination

--- src/utils/appConstants.ts
+++ src/utils/appConstants.ts
@@ -2,3 +2,5 @@ import transactionsData from "../data/transactions.json" with { type: "json" };
 
 // Setting the date of the last mock transaction as the current date (used instead of 'new Date()'):
 export const TODAY = new Date(transactionsData.at(-1)!.date as string);
+
+export const DATA_ROWS_PER_PAGE = 15;

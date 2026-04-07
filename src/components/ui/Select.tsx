d097df920b602a9320135ebccea348999300f41a
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-07 16:45:59 +0530

    Increased font size for larger screens

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -14,7 +14,7 @@ export default function Select({ options, value, onChange, ...props }: SelectInp
     <select
       value={value}
       onChange={onChange}
-      className="px-2 py-1 bg-component-bg border-r-2 border-component-bg text-xs rounded-sm font-semibold shadow-[0_3px_5px_1px_rgba(0,0,0,0.06)]"
+      className="px-2 py-1 bg-component-bg border-r-2 border-component-bg text-xs lg:text-sm rounded-sm font-semibold shadow-[0_3px_5px_1px_rgba(0,0,0,0.06)]"
       {...props}>
       {options.map((option) => (
         <option value={option.value} key={`${option.label}${option.value}`}>

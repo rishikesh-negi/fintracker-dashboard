f5ebc76169dbfffbdea147a4346a19196980a49d
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-07 19:54:34 +0530

    Removed unused prop

--- src/components/ui/PaginationButton.tsx
+++ src/components/ui/PaginationButton.tsx
@@ -1,13 +1,15 @@
-import type { ComponentPropsWithoutRef } from "react";
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
 
 type PaginationButtonProps = ComponentPropsWithoutRef<"button"> & {
-  active: boolean;
+  children: ReactNode;
 };
 
-export default function PaginationButton({ active, ...props }: PaginationButtonProps) {
+export default function PaginationButton({ children, ...props }: PaginationButtonProps) {
   return (
     <button
       className="flex items-center justify-center gap-1 py-1.5 px-3 border-none rounded-sm font-semibold text-sm cursor-pointer disabled:cursor-not-allowed transition duration-200 [&:has(span:last-child)]:pl-1 [&:has(span:first-child)]:pr-1 [&_svg]:w-4 [&_svg]:h-4 hover:not-disabled:bg-accent-700 hover:not-disabled:text-light-100"
-      {...props}></button>
+      {...props}>
+      {children}
+    </button>
   );
 }

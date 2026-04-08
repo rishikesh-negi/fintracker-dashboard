2434cf9a6d53e1d4a2b25dc6decba0a8c92e1b98
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-08 15:54:12 +0530

    Added style for button's disabled state

--- src/components/ui/PaginationButton.tsx
+++ src/components/ui/PaginationButton.tsx
@@ -7,7 +7,7 @@ type PaginationButtonProps = ComponentPropsWithoutRef<"button"> & {
 export default function PaginationButton({ children, ...props }: PaginationButtonProps) {
   return (
     <button
-      className="flex items-center justify-center gap-1 py-1.5 px-3 border-none rounded-sm font-semibold text-sm cursor-pointer disabled:cursor-not-allowed transition duration-200 [&:has(span:last-child)]:pl-1 [&:has(span:first-child)]:pr-1 [&_svg]:w-4 [&_svg]:h-4 hover:not-disabled:bg-accent-700 hover:not-disabled:text-light-100"
+      className="flex items-center justify-center gap-1 py-1.5 px-3 border-none rounded-sm font-semibold text-sm cursor-pointer disabled:cursor-not-allowed transition duration-200 [&:has(span:last-child)]:pl-1 [&:has(span:first-child)]:pr-1 [&_svg]:w-4 [&_svg]:h-4 hover:not-disabled:bg-accent-700 hover:not-disabled:text-light-100 disabled:text-faint-text"
       {...props}>
       {children}
     </button>

3834c15b8ba772717afbb1af29bf63800af7b8a0
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-09 19:42:04 +0530

    A button to close an open modal

---
+++ src/components/ui/ButtonCloseModal.tsx
@@ -0,0 +1,16 @@
+import type { ComponentPropsWithoutRef, MouseEventHandler } from "react";
+
+type ButtonCloseModalProps = ComponentPropsWithoutRef<"button"> & {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function ButtonCloseModal({ onClick, ...props }: ButtonCloseModalProps) {
+  return (
+    <button
+      className="absolute top-4 right-4 w-6 h-6 flex items-center justify-center aspect-square rounded-full shadow-[0_1px_5px_rgba(0,0,0,0.25)] bg-faint-text text-accent-700 text-md xl:text-lg cursor-pointer hover:bg-accent-700 hover:text-component-bg"
+      onClick={onClick}
+      {...props}>
+      &times;
+    </button>
+  );
+}

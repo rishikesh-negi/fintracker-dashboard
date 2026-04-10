b65ad964ed7e2da9f3c5dfb91a65d99fdfb1dbba
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-10 20:02:11 +0530

    Exposed an API to allow opening the modal dialog from outside the Modal component

--- src/components/ui/ButtonOpenModal.tsx
+++ src/components/ui/ButtonOpenModal.tsx
@@ -1,31 +1,43 @@
-import type { ReactElement } from "react";
+import { useImperativeHandle, type ReactElement, type Ref } from "react";
 import Button from "./Button";
 import { useModal } from "./Modal";
 
 type ButtonOpenModalProps = {
   buttonType: "primary" | "custom";
   label?: string;
-  CustomButton?: ReactElement<HTMLButtonElement>;
+  CustomButton?: ReactElement;
+  ref: Ref<unknown>;
 };
 
 export default function ButtonOpenModal({
   buttonType,
   label,
   CustomButton,
+  ref,
   ...props
 }: ButtonOpenModalProps) {
   const { setModalIsOpen } = useModal();
 
+  useImperativeHandle(ref, () => ({
+    openModal() {
+      setModalIsOpen(true);
+    },
+  }));
+
+  function handleOpenModal() {
+    setModalIsOpen(true);
+  }
+
   if (buttonType === "primary")
     return (
-      <Button onClick={() => setModalIsOpen(true)} {...props}>
+      <Button onClick={handleOpenModal} {...props}>
         {label}
       </Button>
     );
 
   if (buttonType === "custom")
     return (
-      <div role="button" className="grid" onClick={() => setModalIsOpen(true)} {...props}>
+      <div role="button" className="grid" onClick={handleOpenModal} {...props}>
         {CustomButton}
       </div>
     );

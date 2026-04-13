56e724a7c7eac623f393cbf341f376731c98203a
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-13 19:26:25 +0530

    Added styling and updated props type

--- src/components/ui/FormTextInput.tsx
+++ src/components/ui/FormTextInput.tsx
@@ -3,15 +3,28 @@ import type { ComponentPropsWithRef, Ref } from "react";
 type FormInputProps = ComponentPropsWithRef<"input"> & {
   label?: string;
   placeholder?: string;
+  type: "text" | "number";
   id: string;
-  ref: Ref<HTMLInputElement>;
+  ref?: Ref<HTMLInputElement>;
 };
 
-export default function FormInput({ label, placeholder, id, name, ref, ...props }: FormInputProps) {
+export default function FormTextInput({
+  label,
+  placeholder,
+  type,
+  id,
+  name,
+  ref,
+  ...props
+}: FormInputProps) {
   return (
-    <p>
-      {label && <label htmlFor={id}>{label}</label>}
-      <input type="text" placeholder={placeholder} id={id} name={name} ref={ref} {...props} />
+    <p className="w-full flex flex-col gap-1">
+      {label && (
+        <label htmlFor={id} className="text-xs sm:text-sm">
+          {label}:
+        </label>
+      )}
+      <input type={type} placeholder={placeholder} id={id} name={name} ref={ref} {...props} />
     </p>
   );
 }

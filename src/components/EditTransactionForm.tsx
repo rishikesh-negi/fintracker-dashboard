f40fc94e2bf5dfd28acd57b93045dfb2c86660aa
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-14 20:48:08 +0530

    Added form inputs and logic to handle submission

--- src/components/EditTransactionForm.tsx
+++ src/components/EditTransactionForm.tsx
@@ -1,9 +1,14 @@
-import { useRef } from "react";
+import { useRef, useState, type ChangeEvent } from "react";
+import { editTransaction, type Transaction, type TransactionCategory } from "../store/accountSlice";
+import { useAppDispatch } from "../store/storeHooks";
+import { useMenu } from "./Menu";
+import Button from "./ui/Button";
+import ButtonCloseModal from "./ui/ButtonCloseModal";
 import Form, { type FormApi } from "./ui/Form";
-import type { Transaction } from "../store/accountSlice";
+import FormSelectInput from "./ui/FormSelectInput";
 import FormTextInput from "./ui/FormTextInput";
 import { useModal } from "./ui/Modal";
-import ButtonCloseModal from "./ui/ButtonCloseModal";
+import { getCategoriesByTransactionType } from "../utils/getCategoriesByTransactionType";
 
 type EditTransactionFormProps = {
   transaction: Transaction;
@@ -11,29 +16,84 @@ type EditTransactionFormProps = {
 
 export default function EditTransactionForm({ transaction }: EditTransactionFormProps) {
   const form = useRef<FormApi>(null);
+  const [transactionType, setTransactionType] = useState<"income" | "expense">(
+    transaction.transactionType,
+  );
+  const transactionTypeRef = useRef<"income" | "expense">(transactionType);
+  const [categoryOptions, setCategoryOptions] = useState<{ label: string; value: string }[]>(
+    getCategoriesByTransactionType(transaction.transactionType),
+  );
+  const dispatch = useAppDispatch();
+  const { setModalIsOpen } = useModal();
+  const { close } = useMenu();
 
-  function onFormSubmit() {
+  function onSave(value: {
+    amount: string;
+    description: string;
+    transactionType: "income" | "expense";
+    category: TransactionCategory;
+  }) {
+    dispatch(editTransaction({ id: transaction.transactionId, ...value }));
     form.current?.clear();
+    setModalIsOpen(false);
+    close();
   }
 
-  const { setModalIsOpen } = useModal();
+  function handleTransactionTypeChange(e: ChangeEvent<HTMLSelectElement, HTMLSelectElement>) {
+    transactionTypeRef.current = e.target.value as "income" | "expense";
+    setCategoryOptions(getCategoriesByTransactionType(transactionTypeRef.current));
+  }
 
   return (
     <Form
       ref={form}
-      onSave={onFormSubmit}
-      className="component-container border border-accent-900 flex flex-col gap-2 lg:gap-3 w-full h-fit text-text">
-      <h4 className="component-heading mb-2 lg:my-4">
+      onSave={onSave as <T>(value: T) => void}
+      className="component-container w-[80dvw] md:w-[60dvw] lg:w-[40dvw] xl:w-[30dvw] h-fit flex flex-col gap-4 lg:gap-6 shadow-lg text-text">
+      <h4 className="component-heading mb-2 lg:mb-0">
         <ButtonCloseModal onClick={() => setModalIsOpen(false)} />
         Edit transaction {transaction.transactionId}
       </h4>
       <FormTextInput
-        label="amount"
+        label="Amount"
         type="number"
-        id="transaction-amount"
-        name="transaction-amount"
-        className="w-full px-2 md:px-3 py-1 md:py-1.5 rounded-sm text-sm md:text-lg bg-backdrop outline-0 border border-accent-900 number-input-reset focus-visible:border-accent-500"
+        step="any"
+        id="amount"
+        name="amount"
+        className="edit-transaction-form-input number-input-reset"
+        defaultValue={transaction.amount}
       />
+      <FormTextInput
+        label="Description"
+        type="text"
+        id="description"
+        name="description"
+        className="edit-transaction-form-input"
+        defaultValue={transaction.description}
+      />
+      <FormSelectInput
+        label="Transaction type"
+        id="transactionType"
+        name="transactionType"
+        options={[
+          { label: "Income", value: "income" },
+          { label: "Expense", value: "expense" },
+        ]}
+        className="edit-transaction-form-input"
+        defaultValue={transactionType}
+        onChange={handleTransactionTypeChange}
+      />
+      <FormSelectInput
+        label="Category"
+        id="category"
+        name="category"
+        options={categoryOptions}
+        className="edit-transaction-form-input"
+        defaultValue={transactionType}
+        onChange={(e) => setTransactionType(e.target.value as "income" | "expense")}
+      />
+      <Button className="w-fit px-4 py-1 ml-auto rounded-sm bg-accent-700 hover:bg-accent-900 text-light-50 text-xs lg:text-sm font-semibold cursor-pointer tracking-wide">
+        Edit
+      </Button>
     </Form>
   );
 }

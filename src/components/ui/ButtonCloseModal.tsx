5ff1c38705e26ab77be5bc894ae4d955bd07959d
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-14 20:49:22 +0530

    Improved button styling, position, and appearance

--- src/components/ui/ButtonCloseModal.tsx
+++ src/components/ui/ButtonCloseModal.tsx
@@ -7,7 +7,7 @@ type ButtonCloseModalProps = ComponentPropsWithoutRef<"button"> & {
 export default function ButtonCloseModal({ onClick, ...props }: ButtonCloseModalProps) {
   return (
     <button
-      className="absolute top-4 right-4 w-6 h-6 flex items-center justify-center aspect-square rounded-full shadow-[0_1px_5px_rgba(0,0,0,0.25)] border border-accent-700 text-accent-700 text-md xl:text-lg cursor-pointer hover:bg-accent-700 hover:text-component-bg duration-75"
+      className="absolute top-2 md:top-4 right-2 md:right-4 w-5 md:w-6 h-5 md:h-6 aspect-square flex items-center justify-center rounded-full shadow-[0_3px_3px_1px_rgba(0,0,0,0.15)] text-text/60 text-sm md:text-lg xl:text-2xl cursor-pointer hover:bg-accent-700 hover:text-component-bg hover:shadow-none transition-all duration-75"
       onClick={onClick}
       {...props}>
       &times;
